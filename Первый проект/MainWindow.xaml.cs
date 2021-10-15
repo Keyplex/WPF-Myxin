43179a26edbd223b5b1629edac53d763315a68e7
Pavel <51826654+1commando1@users.noreply.github.com>
2021-10-15 15:18:18 +0300

    Добавьте файлы проекта.

---
+++ Первый проект/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using Первый_проект.Программа;
+
+namespace Первый_проект
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            frmMain.Navigate(new LogPar());
+            PageLoad.Load = frmMain;
+            //frmMain.Navigate(new Page1());
+            BaseConnect.BaseModel = new Entities1();
+
+
+        }
+        
+
+        //private void Button_Click(object sender, RoutedEventArgs e) //Записать 
+        //{
+
+        //    string path = @"Text.txt";
+        //    string dop = "";
+        //    if (chbKind.IsChecked == true)
+        //    {
+        //        dop += (chbKind.Content) + ";";
+        //    }
+        //    if (chbCommunicate.IsChecked == true)
+        //    {
+        //        dop += (chbCommunicate.Content) + ";";
+        //    }
+        //    if (chbModest.IsChecked == true)
+        //    {
+        //        dop += (chbModest.Content) + ";";
+        //    }
+
+        //    //FileInfo f1 = new FileInfo(path);
+        //    //using (StreamWriter sw = f1.AppendText())
+        //    //{
+        //    //    //sw.WriteLine("Hello");
+        //    //    sw.WriteLine(txtName.Text);
+        //    //    sw.WriteLine(DataRozh.Text);
+        //    //    ListBoxItem li = (ListBoxItem)Vibrat.SelectedItem;
+        //    //    sw.WriteLine(li.Content);//это почти тоже, что и text
+        //    //    sw.WriteLine(dop);
+        //    //}
+        //    using (BinaryWriter sw = new BinaryWriter(File.Open(path, FileMode.Append)))
+        //    {
+        //        sw.Write(txtName.Text);
+        //        sw.Write(DataRozh.Text);
+        //        //ListBoxItem li = (ListBoxItem)Vibrat.SelectedItem;
+        //        //sw.WriteLine(li.Content);//это почти тоже, что и text
+        //        ListBoxItem li = (ListBoxItem)Vibrat.SelectedItem;
+        //        //sw.Write(li.);
+        //        sw.Write(dop);
+        //    }
+
+
+        //    //надо создавать отдельно элемент списка. типа listboxitem и уже у него есть свойство text. вроде в каком-то из моих видео есть это
+        //    //StreamReader sr = f1.OpenText();
+        //}
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) //Прочитать
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Первый проект/Программа/PageUsersLisl.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Первый_проект.Программа
+{
+    /// <summary>
+    /// Логика взаимодействия для PageUsersLisl.xaml
+    /// </summary>
+    public partial class PageUsersLisl : Page
+    {
+        List<users> users;
+        public PageUsersLisl()
+        {
+            InitializeComponent();
+            users = BaseConnect.BaseModel.users.ToList();
+            //lbUsersList.ItemsSource = BaseConnect.BaseModel.users.ToList();
+            lbUsersList.ItemsSource = users;
+        }
+
+
+        private void lbTraits_Loaded(object sender, RoutedEventArgs e)
+        {
+            ListBox lb = (ListBox)sender;
+            //lb.Items.Add("Проверка связи");
+            int index = Convert.ToInt32(lb.Uid);
+            lb.ItemsSource = BaseConnect.BaseModel.users_to_traits.Where(x => x.id_user == index).ToList();
+            lb.DisplayMemberPath = "traits.trait";
+        }
+
+        private void lbUsersList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            int OT = Convert.ToInt32(txtOT.Text);
+            int DO = Convert.ToInt32(txtDO.Text);
+            List<users> lu1 = users.Skip(OT).Take(DO - OT).ToList(); // skip - пропустить определенное количество записей , take - выбрать определенное количество записей
+            lbUsersList.ItemsSource = lu1;
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            lbUsersList.ItemsSource = users; //в качестве источника данных новый список
+        }
+    }
+}
