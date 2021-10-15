738b0d31c7718d2567af2247102726dc4def284e
Pavel <51826654+1commando1@users.noreply.github.com>
2021-10-15 15:18:18 +0300

    Добавьте файлы проекта.

---
+++ Первый проект/Программа/adminMenu.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Первый_проект.Программа
+{
+    /// <summary>
+    /// Логика взаимодействия для adminMenu.xaml
+    /// </summary>
+    public partial class adminMenu : Page
+    {
+        public adminMenu()
+        {
+            InitializeComponent();
+            dgUsers.ItemsSource = BaseConnect.BaseModel.auth.ToList();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            PageLoad.Load.GoBack();
+        }
+
+        private void btnEditUser_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вы нажали изменили");
+        }
+
+        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            auth SelectedUser = (auth)dgUsers.SelectedItem;
+            BaseConnect.BaseModel.auth.Remove(SelectedUser);
+            BaseConnect.BaseModel.SaveChanges();
+            MessageBox.Show("Выбранный пользователь удален");
+            dgUsers.ItemsSource = BaseConnect.BaseModel.auth.ToList();
+        }
+
+        private void btnSaveCahanges_Click(object sender, RoutedEventArgs e)
+        {
+            BaseConnect.BaseModel.SaveChanges();
+            MessageBox.Show("Вы нажали сохранять");
+        }
+    }
+}
